82014b6f208b1c6c6cf211c577604ff02520fa86
roadblock37 <Roadblock37@gmail.com>
2022-11-07 14:54:51 -0500

    basic tile built with styling

--- src/components/tile.js
+++ src/components/tile.js
@@ -1,6 +1,30 @@
-const Tile = ({id, logo, logoBackground, postedAt, contract, position, company, location}) => {
-    return (
-        <div>Testing</div>
-    );
-}
+const Tile = ({
+  logo,
+  logoBackground,
+  postedAt,
+  contract,
+  position,
+  company,
+  location,
+}) => {
+  return (
+    //   tile container
+    <div className="tile">
+      {/*container that hold company logo */}
+      <div className="logo-container" style={(background = { logoBackground })}>
+        <img src={logo} alt={company} />
+      </div>
+      {/*section holding position details */}
+      <section className="position-container">
+        <div className="posted-container">
+          <h4>{postedAt}</h4>
+          <h4>{contract}</h4>
+        </div>
+        <h3>{position}</h3>
+        <h4>{company}</h4>
+      </section>
+      <h3 className="location">{location}</h3>
+    </div>
+  );
+};
 export default Tile;
