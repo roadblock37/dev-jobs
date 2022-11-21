9c9076c5ed0bd52493ce5f38ed3a3c29e7923489
Paul Biber <roadblock37@gmail.com>
2022-11-21 12:39:16 -0500

    resolved merge conlficts

--- src/App.js
+++ src/App.js
@@ -2,19 +2,7 @@ import Tile from "./components/tile";
 import TileContainer from "./components/tileContainer";
 import { useSelector } from "react-redux";
 function App() {
-<<<<<<< HEAD
   return <TileContainer />;
-=======
-  const { defaultState } = useSelector((store) => store.jobs);
-  return (
-    <>
-      {defaultState.map((job) => {
-        console.log(defaultState);
-        return <Tile key={job.id} {...job} />;
-      })}
-    </>
-  );
->>>>>>> 5030d845433af9b9a3a01fa56111a513ace5e41b
 }
 
 export default App;
